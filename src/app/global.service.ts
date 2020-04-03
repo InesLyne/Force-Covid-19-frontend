001d48f9939a506e990030284627f6dd645e5d58
Ngor THIAM <thiam.ngor@gmail.com>
2020-04-03 20:21:19 +0200

    fix sort direction.

--- src/app/global.service.ts
+++ src/app/global.service.ts
@@ -41,7 +41,7 @@ export class GlobalService {
       url = url + '&key=' + filter.key.trim() +'&operator=' + filter.operation.trim()+ '&value=' + filter.value;
     }
     if(filter.orderBy && filter.orderBy.length){
-      url = url + '&orderBy=' + filter.orderBy.trim()+ '&orderDirection=' + filter.orderDirection.trim(); 
+      url = url + '&orderBy=' + filter.orderBy.trim()+ '&orderDirection=' + filter.orderDirection; 
     }
     if(filter.searchKey && filter.searchKey.length){
       url = url + '&searchKey=' + filter.searchKey.trim(); 
